6f2fefebc99ff69c3511f166d8991405279dbbd1
Tanishq <tanishhhh38@gmail.com>
2024-06-26 15:16:17 +0530

    latest updates are uploaded

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,19 @@
-function getCookie(name){
-    let cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-      let cookiePair = cookieArr[i].split("=");
-      if (name == cookiePair[0].trim()) {
-        return decodeURIComponent(cookiePair[1]);
-      }
-    }
-    return null;
-}
+// function getCookie(name){
+//     let cookieArr = document.cookie.split(";");
+//     for (let i = 0; i < cookieArr.length; i++) {
+//       let cookiePair = cookieArr[i].split("=");
+//       if (name == cookiePair[0].trim()) {
+//         return decodeURIComponent(cookiePair[1]);
+//       }
+//     }
+//     return null;
+// }
 
 $(document).ready(function () {
-    
+  $("#home-page-login-heading").hide();
+
+  var cookieValue = $.cookie("sessionToken");
+  if (cookieValue) {
+    $("#home-page-login-heading").show();
+  }
 });
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   $("#register-page-error-alert").hide();
+
+  $("#register-page-email").keypress(function () {
+    $("#register-page-error-alert").hide();
+  });
+
   $("#registerForm").on("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
     var user = {};
@@ -8,8 +13,6 @@ $(document).ready(function () {
     user.email = $("#register-page-email").val();
     user.password = $("#register-page-password").val();
 
-    console.log(user);
-
     $.ajax({
       type: "POST",
       url: "http://localhost:5000/api/users/register",
@@ -17,10 +20,18 @@ $(document).ready(function () {
       contentType: "application/json",
       dataType: "json",
       success: function (response) {
-        console.log(response);
+        setSessionCookie(response.token);
+        document.href = "../index.html";
+      },
+      error: function (response) {
+        $("#register-page-error-alert").show();
       },
     });
   });
 });
+
+function setSessionCookie(token) {
+  $.cookie("sessionToken", token, { expires: 7, path: "/" });
+}
 
 
